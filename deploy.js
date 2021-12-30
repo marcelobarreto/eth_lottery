33be4f637048f02487bbe7792604811e9729aa61
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-12-30 14:45:01 -0300

    fix deploy script

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 const config = require('./config');
 
 const mnemonic = 'enforce flock mutual problem hobby sting good trouble pelican flame honey belt';
@@ -14,8 +14,8 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
